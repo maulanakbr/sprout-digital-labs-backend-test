b0fdc4b4fe482da93260db110cd2754e4b4afb90
Maulana Akbar Yudistika <yudistika.akbar@gmail.com>
2025-05-02 23:40:28 +0700

    chore: add mocks dir inside pic module

--- __tests__/piece.test.ts
+++ __tests__/piece.test.ts
@@ -1,5 +1,6 @@
 import type { Grid } from '../src/types';
 import { MockPiece } from '../src/models/pieces/mocks/mock-piece';
+import { DIMENSION } from '../src/utils/constants';
 
 describe('Piece (abstract)', () => {
   test('should store color and symbol correctly', () => {
@@ -11,7 +12,7 @@ describe('Piece (abstract)', () => {
   test('should allow subclass to implement canMove', () => {
     const piece = new MockPiece('black');
 
-    const mockBoard: Grid = Array.from({ length: 8 }, () => Array(8).fill(null));
+    const mockBoard: Grid = Array.from({ length: DIMENSION }, () => Array(DIMENSION).fill(null));
     const result = piece.canMove([0, 0], [1, 0], mockBoard);
 
     expect(result).toBe(true);
